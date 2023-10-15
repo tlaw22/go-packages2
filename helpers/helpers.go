9b3e442918dc4f572622c23d81f776227d94e4e5
netadmin <timothy.j.lawless@outlook.com>
2023-10-15 08:44:15 -0400

    added random and time function sto the helper class and used channels to pass data

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,8 +1,19 @@
 package helpers
 
+import (
+	"math/rand"
+	"time"
+)
+
 // Create a generic type just to test the included package.
 // Remember that the type name must start with a capital letter
 type SomeType struct {
 	TypeName string
 	TypeNum  int
+}
+
+func RandomNumber(n int) int {
+	rand.Seed(time.Now().UnixMicro())
+	value := rand.Intn(n)
+	return value
 }
--- main.go
+++ main.go
@@ -7,6 +7,18 @@ import (
 	"github.com/tlaw22/go-packages2/helpers"
 )
 
+// type go mod init github.com/tlaw22/go-packages2/helpers
+const numPool = 1000
+
+func PrintText(s string) {
+	log.Println(s)
+}
+
+// create a function that used the RandomNumber function from helpers package
+func CalculateValue(intChan chan int) {
+	randomNumber := helpers.RandomNumber(numPool)
+	intChan <- randomNumber
+}
 func main() {
 	log.Println("Beging app...")
 	// Start referencing the the package by typing the package name and then a period .
@@ -14,4 +26,12 @@ func main() {
 	myVar.TypeName = "Some Name"
 	myVar.TypeNum = 9
 	log.Println(myVar)
+	PrintText("Program complete...")
+
+	intChan := make(chan int)
+	defer close(intChan) // close the channel
+
+	go CalculateValue(intChan)
+	num := <-intChan
+	log.Println(num)
 }
